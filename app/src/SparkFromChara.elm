3ee00311868e78225c540237bfe2829381b8d8d0
ossan <ossan.dry.tomato@gmail.com>
2019-07-18 14:39:07 +0900

    add: テストコード。ボタンに設定されるクラスを検証

--- app/src/SparkFromChara.elm
+++ app/src/SparkFromChara.elm
@@ -1,4 +1,4 @@
-module SparkFromChara exposing (main)
+module SparkFromChara exposing (Model, SelectedWeaponTypes, main, view)
 
 import Browser
 import Dict exposing (Dict)
---
+++ app/tests/SparkFromCharaTest.elm
@@ -0,0 +1,117 @@
+module SparkFromCharaTest exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import SparkFromChara exposing (..)
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (classes, tag, text)
+
+
+suite : Test
+suite =
+    let
+        unselectedAllWeaponTypes : SelectedWeaponTypes
+        unselectedAllWeaponTypes =
+            { sword = False
+            , greatSword = False
+            , axe = False
+            , mace = False
+            , spear = False
+            , shortSword = False
+            , bow = False
+            , martialSkill = False
+            }
+
+        selectedAllWeaponTypes : SelectedWeaponTypes
+        selectedAllWeaponTypes =
+            { sword = True
+            , greatSword = True
+            , axe = True
+            , mace = True
+            , spear = True
+            , shortSword = True
+            , bow = True
+            , martialSkill = True
+            }
+    in
+    describe "The SparkFromChara module"
+        [ describe "view"
+            [ test "剣の技が表示対象の場合、剣のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "剣" "selected" <|
+                        { unselectedAllWeaponTypes | sword = True }
+            , test "剣の技が表示対象外の場合、剣のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "剣" "unselected" <|
+                        { selectedAllWeaponTypes | sword = False }
+            , test "大剣の技が表示対象の場合、大剣のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "大剣" "selected" <|
+                        { unselectedAllWeaponTypes | greatSword = True }
+            , test "大剣の技が表示対象外の場合、大剣のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "大剣" "unselected" <|
+                        { selectedAllWeaponTypes | greatSword = False }
+            , test "斧の技が表示対象の場合、斧のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "斧" "selected" <|
+                        { unselectedAllWeaponTypes | axe = True }
+            , test "斧の技が表示対象外の場合、斧のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "斧" "unselected" <|
+                        { selectedAllWeaponTypes | axe = False }
+            , test "棍棒の技が表示対象の場合、棍棒のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "棍棒" "selected" <|
+                        { unselectedAllWeaponTypes | mace = True }
+            , test "棍棒の技が表示対象外の場合、棍棒のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "棍棒" "unselected" <|
+                        { selectedAllWeaponTypes | mace = False }
+            , test "槍の技が表示対象の場合、槍のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "槍" "selected" <|
+                        { unselectedAllWeaponTypes | spear = True }
+            , test "槍の技が表示対象外の場合、槍のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "槍" "unselected" <|
+                        { selectedAllWeaponTypes | spear = False }
+            , test "小剣の技が表示対象の場合、小剣のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "小剣" "selected" <|
+                        { unselectedAllWeaponTypes | shortSword = True }
+            , test "小剣の技が表示対象外の場合、小剣のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "小剣" "unselected" <|
+                        { selectedAllWeaponTypes | shortSword = False }
+            , test "弓の技が表示対象の場合、弓のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "弓" "selected" <|
+                        { unselectedAllWeaponTypes | bow = True }
+            , test "弓の技が表示対象外の場合、弓のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "弓" "unselected" <|
+                        { selectedAllWeaponTypes | bow = False }
+            , test "体術の技が表示対象の場合、体術のボタンに selected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "体術" "selected" <|
+                        { unselectedAllWeaponTypes | martialSkill = True }
+            , test "体術の技が表示対象外の場合、体術のボタンに unselected クラスを設定する" <|
+                \_ ->
+                    verifyClassOfButton "体術" "unselected" <|
+                        { selectedAllWeaponTypes | martialSkill = False }
+            ]
+        ]
+
+
+verifyClassOfButton : String -> String -> SelectedWeaponTypes -> Expectation
+verifyClassOfButton weaponType className selectedWeaponTypes =
+    Model selectedWeaponTypes
+        |> view
+        |> Query.fromHtml
+        |> Query.find [ classes [ "weapon-type-filter" ] ]
+        -- find [ text weaponType ] の後に has [ tag ... ] としたいが、text "剣" に対して "大剣" と "小剣" がマッチしてしまうため find と has の条件を入れ替えている。
+        -- このため、この関数では className が設定される要素は 1つのみという前提になっている。
+        |> Query.find [ tag "button" , classes [ className ] ]
+        |> Query.has [ text weaponType ]
