ac4cc25932a6bc9392db1a22b0757073b8248a44
ossan <ossan.dry.tomato@gmail.com>
2019-07-19 02:14:07 +0900

    refactor: テスト名

--- app/tests/SparkFromCharaTest.elm
+++ app/tests/SparkFromCharaTest.elm
@@ -37,67 +37,67 @@ suite =
     in
     describe "The SparkFromChara module"
         [ describe "view"
-            [ test "剣の技が表示対象の場合、剣のボタンに selected クラスを設定する" <|
+            [ test "剣の技が表示対象の場合、剣ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "剣" "selected" <|
                         { unselectedAllWeaponTypes | sword = True }
-            , test "剣の技が表示対象外の場合、剣のボタンに unselected クラスを設定する" <|
+            , test "剣の技が表示対象外の場合、剣ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "剣" "unselected" <|
                         { selectedAllWeaponTypes | sword = False }
-            , test "大剣の技が表示対象の場合、大剣のボタンに selected クラスを設定する" <|
+            , test "大剣の技が表示対象の場合、大剣ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "大剣" "selected" <|
                         { unselectedAllWeaponTypes | greatSword = True }
-            , test "大剣の技が表示対象外の場合、大剣のボタンに unselected クラスを設定する" <|
+            , test "大剣の技が表示対象外の場合、大剣ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "大剣" "unselected" <|
                         { selectedAllWeaponTypes | greatSword = False }
-            , test "斧の技が表示対象の場合、斧のボタンに selected クラスを設定する" <|
+            , test "斧の技が表示対象の場合、斧ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "斧" "selected" <|
                         { unselectedAllWeaponTypes | axe = True }
-            , test "斧の技が表示対象外の場合、斧のボタンに unselected クラスを設定する" <|
+            , test "斧の技が表示対象外の場合、斧ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "斧" "unselected" <|
                         { selectedAllWeaponTypes | axe = False }
-            , test "棍棒の技が表示対象の場合、棍棒のボタンに selected クラスを設定する" <|
+            , test "棍棒の技が表示対象の場合、棍棒ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "棍棒" "selected" <|
                         { unselectedAllWeaponTypes | mace = True }
-            , test "棍棒の技が表示対象外の場合、棍棒のボタンに unselected クラスを設定する" <|
+            , test "棍棒の技が表示対象外の場合、棍棒ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "棍棒" "unselected" <|
                         { selectedAllWeaponTypes | mace = False }
-            , test "槍の技が表示対象の場合、槍のボタンに selected クラスを設定する" <|
+            , test "槍の技が表示対象の場合、槍ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "槍" "selected" <|
                         { unselectedAllWeaponTypes | spear = True }
-            , test "槍の技が表示対象外の場合、槍のボタンに unselected クラスを設定する" <|
+            , test "槍の技が表示対象外の場合、槍ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "槍" "unselected" <|
                         { selectedAllWeaponTypes | spear = False }
-            , test "小剣の技が表示対象の場合、小剣のボタンに selected クラスを設定する" <|
+            , test "小剣の技が表示対象の場合、小剣ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "小剣" "selected" <|
                         { unselectedAllWeaponTypes | shortSword = True }
-            , test "小剣の技が表示対象外の場合、小剣のボタンに unselected クラスを設定する" <|
+            , test "小剣の技が表示対象外の場合、小剣ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "小剣" "unselected" <|
                         { selectedAllWeaponTypes | shortSword = False }
-            , test "弓の技が表示対象の場合、弓のボタンに selected クラスを設定する" <|
+            , test "弓の技が表示対象の場合、弓ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "弓" "selected" <|
                         { unselectedAllWeaponTypes | bow = True }
-            , test "弓の技が表示対象外の場合、弓のボタンに unselected クラスを設定する" <|
+            , test "弓の技が表示対象外の場合、弓ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "弓" "unselected" <|
                         { selectedAllWeaponTypes | bow = False }
-            , test "体術の技が表示対象の場合、体術のボタンに selected クラスを設定する" <|
+            , test "体術の技が表示対象の場合、体術ボタンに selected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "体術" "selected" <|
                         { unselectedAllWeaponTypes | martialSkill = True }
-            , test "体術の技が表示対象外の場合、体術のボタンに unselected クラスを設定する" <|
+            , test "体術の技が表示対象外の場合、体術ボタンに unselected クラスを設定する" <|
                 \_ ->
                     verifyClassOfButton "体術" "unselected" <|
                         { selectedAllWeaponTypes | martialSkill = False }
