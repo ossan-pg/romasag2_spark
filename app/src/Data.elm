625529d3dae406e7e7494c790937a82fb7c66499
ossan-pg <ossan.dry.tomato@gmail.com>
2019-08-07 01:01:57 +0900

    feat: 「閃き可能な技」の右側にキャラクターの閃きタイプを表示する

--- app/src/Data.elm
+++ app/src/Data.elm
@@ -1,13 +1,13 @@
 module Data exposing
     ( CharaClassSymbol(..), CharaClass, charaClasses
-    , SparkTypeSymbol(..), Chara, charas
+    , SparkTypeSymbol(..), sparkTypeToName, Chara, charas
     , WeaponTypeSymbol(..), Waza, wazas, sparkTypeToWazas
     )
 
 {-|
 
 @docs CharaClassSymbol, CharaClass, charaClasses
-@docs SparkTypeSymbol, Chara, charas
+@docs SparkTypeSymbol, sparkTypeToName, Chara, charas
 @docs WeaponTypeSymbol, Waza, wazas, sparkTypeToWazas
 
 -}
@@ -113,6 +113,55 @@ type SparkTypeSymbol
     | SparkGeneral -- 汎用
     | SparkSpell --術
     | SparkNothing --なし
+
+
+sparkTypeToName : SparkTypeSymbol -> String
+sparkTypeToName symbol =
+    case symbol of
+        SparkSword1 ->
+            "剣1"
+
+        SparkSword2 ->
+            "剣2"
+
+        SparkGreatSword1 ->
+            "大剣1"
+
+        SparkGreatSword2 ->
+            "大剣2"
+
+        SparkAxe ->
+            "斧"
+
+        SparkSpearAxe ->
+            "槍斧"
+
+        SparkMace ->
+            "棍棒"
+
+        SparkSpear ->
+            "槍"
+
+        SparkShortSword ->
+            "小剣"
+
+        SparkBow ->
+            "弓"
+
+        SparkMartialSkill1 ->
+            "体術1"
+
+        SparkMartialSkill2 ->
+            "体術2"
+
+        SparkGeneral ->
+            "汎用"
+
+        SparkSpell ->
+            "術"
+
+        SparkNothing ->
+            "なし"
 
 
 type alias Chara =
